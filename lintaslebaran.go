820345cc900d8e35b3afae4424a3bd9d7c3c16a9
veedyf <vidifadilakbar20@gmail.com>
2019-11-20 21:25:00 +0700

    lintas lebaran done

--- lintaslebaran.go
+++ lintaslebaran.go
@@ -12,25 +12,26 @@ var (
 )
 
 func catatLaluLintas(arr *Trafik, n int) {
-	for i := 1; i <= n; i++ {
+	for i := 1; i <= n+1; i++ {
 		fmt.Scan(&arr[i])
 	}
 }
 
 func kendaraanTerbanyak(arr *Trafik, n int) int {
-	var max, idx int
-
+	var max, idx, banyak int
+	max = -9999
 	for i, a := range arr {
-		if a > max {
+		if a > i-1 {
+			banyak += 1
 			max = a
 			idx = i
 		}
 	}
-	fmt.Println("\n tipe kendaraan:", idx)
+	fmt.Println("kendaraan terbanyak:", idx)
+	fmt.Println("Sebanyak :", banyak)
 	return max
 }
 func main() {
 	catatLaluLintas(&arr, 5)
-
-	fmt.Println("Kendaraaan terbanyak :", kendaraanTerbanyak(&arr, 5))
+	kendaraanTerbanyak(&arr, n)
 }
